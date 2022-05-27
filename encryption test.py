c2b7834d93a8214501a792978fdd501fdce1f943
Taylor <100389630+TAys3@users.noreply.github.com>
2022-05-27 14:19:22 +1000

    update

--- encryption test.py
+++ encryption test.py
@@ -1,2 +1,14 @@
 import hashlib
 
+string = "sdd2021"
+encoded = string.encode()
+result = hashlib.sha256(encoded)
+print("String: ", end = "")
+print(string)
+print("Hash Value : ", end ="")
+print(result)
+print("Hexadecimal equivalent: ",result.hexdigest())
+print("Digest Size : ", end ="")
+print(result.digest_size)
+print("Block Size : ", end ="")
+print(result.block_size) 
--- tk_pass_gen.py
+++ tk_pass_gen.py
@@ -117,6 +117,7 @@ pass_len_slider.grid(column=0, row=2, sticky= tk.EW, padx = 20, pady= 10, column
 letters.grid(column=0, row=3, sticky= tk.W, padx = 20, pady= 10)
 cap_letters.grid(column=1, row=3, sticky= tk.W, padx = 20, pady= 10)
 numbers.grid(column=0, row=4, sticky= tk.W, padx = 20, pady= 10)
-
+special_char.grid(column=1, row=4, sticky= tk.W, padx = 20, pady= 10)
+password_entry.grid(column=0, row=5, sticky= tk.EW, padx = 20, pady= 10, columnspan= 2)
 
 root.mainloop()
